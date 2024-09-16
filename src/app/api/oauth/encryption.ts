63381fa68746db65cdedaa933d04c8c5257be942
Trevor Miller <trevormil@comcast.net>
2024-09-16 19:12:41 -0400

    fix cookie encryption

--- src/app/api/oauth/encryption.ts
+++ src/app/api/oauth/encryption.ts
@@ -1,28 +1,14 @@
-import crypto from 'crypto';
-
+import CryptoJS from 'crypto-js';
 const secretKey = process.env.AUTH_SECRET || '';
 
 export const encrypt = async (data: string): Promise<string> => {
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv(
-        'aes-256-cbc',
-        Buffer.from(secretKey),
-        iv
-    );
-    let encrypted = cipher.update(data, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
-    return iv.toString('hex') + ':' + encrypted;
+    const encrypted = CryptoJS.AES.encrypt(data, secretKey).toString();
+    return encrypted;
 };
 
 export const decrypt = async (data: string): Promise<string> => {
-    const [ivHex, encryptedData] = data.split(':');
-    const iv = Buffer.from(ivHex, 'hex');
-    const decipher = crypto.createDecipheriv(
-        'aes-256-cbc',
-        Buffer.from(secretKey),
-        iv
+    const decrypted = CryptoJS.AES.decrypt(data, secretKey).toString(
+        CryptoJS.enc.Utf8
     );
-    let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
     return decrypted;
 };
